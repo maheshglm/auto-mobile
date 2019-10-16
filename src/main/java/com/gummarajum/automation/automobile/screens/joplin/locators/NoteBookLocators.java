6768a7055ba2b89c646de63b0325a566cc6805c4
Mahesh Gummaraju <maheshglm@gmail.com>
2019-10-16 23:42:08 +0800

    Remove getter n setter

--- src/main/java/com/gummarajum/automation/automobile/screens/joplin/locators/NoteBookLocators.java
+++ src/main/java/com/gummarajum/automation/automobile/screens/joplin/locators/NoteBookLocators.java
@@ -9,15 +9,7 @@ public class NoteBookLocators {
 
     @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.view.ViewGroup")
     public MobileElement menu;
-
-    public MobileElement getMenu() {
-        return menu;
-    }
-
-    public void setMenu(MobileElement menu) {
-        this.menu = menu;
-    }
-
+    
     @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[2]/android.view.ViewGroup")
     public MobileElement search;
 
